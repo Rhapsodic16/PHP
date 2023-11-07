d79c6808dc01e81fe4f32d56fe4af70bf0de4847
Rhapsodic16 <eduardo.robles7903@alumnos.udg.mx>
2023-11-07 12:24:28 -0600

    Barra del menu más funcional

--- Biblioteca/Biblioteca.php
+++ Biblioteca/Biblioteca.php
@@ -8,18 +8,23 @@
 </head>
 <body>
     <header class="barraHerramientas">
-        <div class="logo" style="">
-            <img src="" alt="">
-            <h2>Biblioteca Virtual</h2>
+        <div class="contenido">
+            <div class="logo">
+                <img src="" alt="">
+                <h2>Biblioteca Virtual</h2>
+            </div>
+
+            <div class="menu">
+                <nav class="menu">
+                    <ul>
+                        <li class ="seleccionado"><a href="" class="itemSeleccionado">Inicio</a></li>
+                        <li><a href="">Tipo de libro</a></li>
+                        <li><a href="">Registro</a></li>
+                        <li><a href="">Acceder</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
-        <nav class="herramientas">
-            <ul>
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Tipo de libro</a></li>
-                <li><a href="">Registro</a></li>
-                <li><a href="">Acceder</a></li>
-            </ul>
-        </nav>
     </header>
 </body>
 </html>
