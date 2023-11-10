c6874b813afe81cdcd215adacaab175dceaf4c75
Rhapsodic16 <eduardo.robles7903@alumnos.udg.mx>
2023-11-09 23:57:38 -0600

    Todo lo de la base de datos y diseño del CRUD.

---
+++ Biblioteca/database.php
@@ -0,0 +1,40 @@
+<?php
+
+  $servername = "172.21.1.17"; // Servidor de la base de datos
+  $username = "root"; // Nombre de usuario de la base de datos
+  $password = "1234"; // Contraseña de la base de datos
+  $database = "bibliotecavirtual"; // Nombre de la base de datos
+  
+  $dsn = "mysql:host=$servername;dbname=$database;charset=utf8mb4";
+
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+  ];
+
+  $conn = new PDO($dsn, $username, $password, $options);
+
+
+  function executeQuery($isSelect, $query, $values = array()) {
+    
+    global $conn;
+
+    try {
+      $stmt = $conn->prepare($query);
+
+      $stmt->execute($values);
+
+      if ($isSelect) {
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+      return $stmt->rowCount();
+    }
+    catch (PDOException $e) {
+      echo "Error: ". $e->getMessage() ."";
+      return false;
+    }
+  }
+
+?>
--- Prueba.php
+++ Prueba.php
@@ -31,34 +31,7 @@
     ?>
     
     <?php 
-        $servername = "localhost";
-        $database = "alumnos";
-        $username = "root";
-        $password = "";
-        //Create connection
-        $conn = mysqli_connect($servername, $username, $password, $database);
-        //Check connection
-        if(!$conn){
-            die("Connection failed: " . mysqli_connect_error());
-        }
-
-        echo "Connected successfully <br>";
-       
-        
-        $sql = "SELECT Ncontrol, Clave, Calif FROM cursa";
-        $resultado = $conn->query($sql);
-        
-        // Verifica si hay resultados y muestra los datos
-        if ($resultado->num_rows > 0) {
-            // Itera sobre los resultados y muestra cada fila de datos
-            while($fila = $resultado->fetch_assoc()) {
-                echo "Columna1: " . $fila["Ncontrol"]. " - Columna2: " . $fila["Clave"]. " - Columna3: " . $fila["Calif"]. "<br>";
-            }
-        } else {
-            echo "0 resultados encontrados";
-        }
-
-        mysqli_close($conn);
+        require_once('../Biblioteca/database.php');
     ?>
 
 </body>
